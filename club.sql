c5b504da28f33fb119cf0a13e464b5ff2df8e0fc
KerlessAyman <164897238+KerlessAyman@users.noreply.github.com>
2024-04-28 23:51:53 +0300

    Add files via upload

---
+++ club.sql
@@ -0,0 +1,17 @@
+#select * from trainer
+#delete from admin
+#select pin from admin
+#alter table trainer 
+#add(trainer_id int primary key auto_increment)
+#drop column id_trainer
+#create table trainer(user_name varchar(30) not null,password varchar(30),name varchar(30),phone_number varchar(30),gmail varchar(60),sport varchar(20),day varchar(30),time varchar(20),experience varchar(30),Constraint pk1 primary key (user_name))
+#select * from trainer_accepted
+#INSERT INTO `club`.`trainer` (`user_name`, `password`, `name`, `phone_number`, `gmail`, `sport`, `day`, `time`, `experience`,`cphone`) VALUES ('iouo', '128885', 'hassan', '01229360785', 'keromihhv@gmail.com', 'football', 'sunday', '12', '1year','01254656846');
+#alter table trainer 
+#add(id_trainer int)
+#delete from trainer_accepted 
+#alter table admin
+#add column id serial
+#create table trainer_accepted(user_name varchar(30) not null,password varchar(30),name varchar(30),phone_number varchar(30),gmail varchar(60),sport varchar(20),day varchar(30),time varchar(20),experience varchar(30),Constraint pk1 primary key (user_name))
+#select * from admin  order by id asc
+#select * from trainer_accepted
